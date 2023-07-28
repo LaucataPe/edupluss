398a0d5010b5b2833c8b6c727bd6a47a78e36627
Laura Perez <laucatape@gmail.com>
2023-07-27 23:20:06 -0500

    fix(client/login) Edit the login styles

--- educlient/src/components/Login.tsx
+++ educlient/src/components/Login.tsx
@@ -2,9 +2,13 @@ import {useState} from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { FaRegEye, FaRegEyeSlash} from 'react-icons/fa'
 import { setLogUser } from '../redux/features/userSlice'
 import { setEmpresa } from '../redux/features/activitiesSlice'
+
+import { Button } from 'primereact/button';
+import { Password } from 'primereact/password';
+//import { LayoutContext } from '../../../layout/context/layoutcontext';
+import { InputText } from 'primereact/inputtext';
 
 function Login() {
     const dispatch = useDispatch()
@@ -13,21 +17,13 @@ function Login() {
       email: '',
       password: ''
     })
-    const [showPassword, setShowPassword] = useState<boolean>(false)
     const [error, setError] = useState()
 
     const handleInputs = (event: React.ChangeEvent<HTMLInputElement>) =>{
       setInputs({...inputs, 
         [event.target.name]: event.target.value})
     }
-
-    const handleShowPassword = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
-      e.preventDefault()
-      setShowPassword(!showPassword)
-    }
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
+    const handleSubmit = async () => {
       if(inputs.email && inputs.password){
         try {
           const {data} = await axios.post('http://localhost:3001/logUser', inputs)
@@ -41,7 +37,7 @@ function Login() {
           }
           alert('Inicio de sesión exitoso')
           if(data.user.tipo === 'admin'){
-            navigate('/admin')
+            navigate('/crud')
           } else{
             navigate('/home')
           }
@@ -55,36 +51,35 @@ function Login() {
 
     return (
       <>
-      <h1 className='text-3xl font-semibold text-center p-10'>Inicia Sesión</h1>
-      <div className='mx-auto px-20'>
-        <form onSubmit={(e) => handleSubmit(e)} className='flex flex-col md:px-8'>
-          <div className='flex flex-col pt-4'>
-            <label className='text-lg'>Correo:</label>
-            <input type="text" name='email' value={inputs.email} onChange={(e) => handleInputs(e)}
-              className='shadow appearance-none border rounded w-full py-2 px-3
-            text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline'/>
-          </div>
-          <div className='flex flex-col pt-4'>
-            <label className="text-lg">Contraseña:</label>
-            <input type={showPassword === true ? "text" : "password"} name='password' 
-            value={inputs.password} onChange={(e) => handleInputs(e)}
-              className='shadow appearance-none border rounded w-full py-2 px-3
-              text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline' 
-              />
-            <button onClick={(e) => handleShowPassword(e)}>
-              {showPassword === true? <FaRegEye className='inset-y-0 right-0 z-40'/> 
-              : <FaRegEyeSlash className='text-2xl text-blue-500'/>}</button>
-          </div>
-          
-          <button className='py-3 px-4 my-5 inline-flex justify-center items-center gap-2 
-          rounded-md bg-blue-100 border border-transparent font-semibold text-blue-500 
-          hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 
-          ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm'
-          
-          >Ingresar</button>
-          <p className=' text-red-600 font-semibold text-lg'>{error}</p>
-        </form>
-      </div>
+      <div className="flex flex-column align-items-center justify-content-center">
+          <div className="w-[40rem] my-20 surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
+            <h1>Inicia Sesión</h1>
+            <div>
+              <label className="block text-900 text-xl font-medium mb-2">
+                Correo
+              </label>
+              <InputText type="text" name='email' placeholder="Ingresar correo" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} 
+                value={inputs.email} onChange={(e) => handleInputs(e)}/>
+                            <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
+                                Contraseña
+                            </label>
+                            <Password name='password' value={inputs.password} onChange={(e) => handleInputs(e)} 
+                            toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"  placeholder="Ingresar contraseña"></Password>
+
+                            <div className="flex align-items-center justify-content-between mb-5 gap-5">
+                                 <div className="flex align-items-center">
+                                    {/* <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox> */}
+                                    <label htmlFor="rememberme1">Recordarme</label>
+                                </div> 
+                                <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
+                                    Olvidaste tu contraseña?
+                                </a>
+                            </div>
+                            <Button label="Ingresar" className="w-full p-3 text-xl" onClick={handleSubmit}></Button>
+                        </div>
+                    </div>
+                    <p>{error}</p>
+            </div>
       </>
     );
   }
