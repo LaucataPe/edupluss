4a1c8f1928d37c70e13132c262d1a608d80a8e3e
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-29 18:11:21 -0300

    feat:(educlient/components) rate activity component

---
+++ educlient/src/components/RateActivity.tsx
@@ -0,0 +1,21 @@
+import { Rating } from "primereact/rating";
+import { useState } from "react";
+
+function RateActivity() {
+  const [ratingValue, setRatingValue] = useState<number | null>(null);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center p-4 bg-gray-200 rounded-lg md:p-6">
+        <h5 className="mb-1 md:text-2xl">Ayudanos a mejorar</h5>
+        <p className=" md:text-lg">Califica esta actividad</p>
+        <Rating
+          value={ratingValue as number}
+          onChange={(e) => setRatingValue(e.value ?? 0)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default RateActivity;
