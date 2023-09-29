2e8573a7144dc98fe9af002dc63bb30536e6a02d
connect-to-people <connecttopeopledigital@gmail.com>
2023-09-29 17:04:23 -0300

    refactor(educlient/pages/Dashboard) Roles Paginated

--- educlient/src/pages/Dashboard.tsx
+++ educlient/src/pages/Dashboard.tsx
@@ -20,6 +20,11 @@ function Dashboard() {
   const roles = useSelector((state: RootState) => state.roles.roles);
   const [showProgressModal, setShowProgressModal] = useState(false);
   const usersByRole: { [roleName: string]: any[] } = {};
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+
+  // Calcula el número total de páginas
+  const totalPages = Math.ceil(Object.keys(usersByRole).length / itemsPerPage);
 
   currentUsers.forEach((user) => {
     const roleId = user.roleId;
@@ -94,8 +99,28 @@ function Dashboard() {
       <div className="w-[100%]">
         <div className="p-5">
           <h3 className="text-xl font-semibold">
-            Progreso general de Actividades:
+            Progreso general de Actividades:{" "}
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button
+                rounded
+                severity="info"
+                icon="pi pi-arrow-left"
+                onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
+                disabled={currentPage === 1}
+                className="btn btn-primary"
+              ></Button>
+              <Button
+                rounded
+                severity="info"
+                icon="pi pi-arrow-right"
+                onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
+                disabled={currentPage === totalPages}
+                className="btn btn-primary"
+                style={{ marginLeft: "10px" }} // Agregamos un margen izquierdo de 10px
+              ></Button>
+            </div>
           </h3>
+
           {showProgressModal && (
             <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
               <div className="bg-white p-0 rounded-md shadow-md">
@@ -106,32 +131,38 @@ function Dashboard() {
               </div>
             </div>
           )}
+
           <div className="flex">
-            {Object.entries(usersByRole).map(([roleName, users]) => (
-              <div key={roleName} className="col-4 col-md-4">
-                <div className=" border-2 shadow-2xl p-4 rounded-2xl">
-                  <h4>Rol: {roleName}</h4>
-                  {users.map((user) => (
-                    <div key={user.id} className="card mb-3 text-center">
-                      <strong>Usuario:</strong> {user.username}
-                      <div className="col-10 col-xl-3 mx-auto">
-                        {/* Aquí deberías tener tu componente ProgressBar */}
-                        <ProgressBar value={value} />
+            {Object.entries(usersByRole)
+              .slice(
+                (currentPage - 1) * itemsPerPage,
+                currentPage * itemsPerPage
+              )
+              .map(([roleName, users]) => (
+                <div key={roleName} className="col-4 col-md-4">
+                  <div className="border-2 shadow-2xl p-4 rounded-2xl">
+                    <h4>Rol: {roleName}</h4>
+                    {users.map((user) => (
+                      <div key={user.id} className="card mb-3 text-center">
+                        <strong>Usuario:</strong> {user.username}
+                        <div className="col-10 col-xl-3 mx-auto">
+                          {/* Aquí deberías tener tu componente ProgressBar */}
+                          <ProgressBar value={value} />
+                        </div>
+                        <div className="col-6 col-xl-3 mx-auto">
+                          {/* Aquí deberías tener tu componente Button */}
+                          <Button
+                            rounded
+                            severity="info"
+                            icon="pi pi-arrow-right"
+                            onClick={handleButtonClick}
+                          ></Button>
+                        </div>
                       </div>
-                      <div className="col-6 col-xl-3 mx-auto">
-                        {/* Aquí deberías tener tu componente Button */}
-                        <Button
-                          rounded
-                          severity="info"
-                          icon="pi pi-arrow-right"
-                          onClick={handleButtonClick}
-                        ></Button>
-                      </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </div>
