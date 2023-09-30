7c5a05bca1b914a20bbeb490b4254ec540af12ca
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-09-29 20:15:49 -0500

    feat(api/userStep):Post to save the employee's completed steps

--- api/src/controllers/UserProgressActivity/postUserProgress.js
+++
@@ -1,7 +0,0 @@
-const { User, Activity, UserActivityStep } = require('../../db.js');
-
-module.exports = async (req, res) => {
-  try {
-    const { userId, activityId, stepId } = req.body;
-  } catch (error) {}
-};
