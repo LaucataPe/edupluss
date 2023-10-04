0dc03471dfcde1ca53d3844fb0e0de72c78aef49
Franco Vedia <francoaresis@gmail.com>
2023-10-04 16:24:50 -0300

    fix(api/models/area): a property companyId was added because it was causing a problem when creating it by not establishing the relationship

--- api/src/models/TestGrade.js
+++ api/src/models/TestGrade.js
@@ -11,22 +11,22 @@ const { DataTypes } = require('sequelize');
       type: DataTypes.INTEGER,
       defaultValue: 0
     },
-    activityId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "Activity",
-        key: "id",
-      },
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "User",
-        key: "id",
-      },
-    },
+    // activityId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: "Activity",
+    //     key: "id",
+    //   },
+    // },
+    // userId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //     model: "User",
+    //     key: "id",
+    //   },
+    // },
   },{
     timestamps: false
   });
