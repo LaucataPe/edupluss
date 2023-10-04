afd9771f1591349c90b5fbb308486f3aafb9121e
Edward Vera <edwardismael1@gmail.com>
2023-10-04 18:51:05 -0300

    feat(educlient/components) WORK IN PROGRESS get data from excel

--- educlient/src/components/Step.tsx
+++ educlient/src/components/Step.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Step } from "../utils/interfaces";
 import { Button } from "primereact/button";
+
+import axios from 'axios';
+import * as XLSX from 'xlsx';
 
 type props = {
   step: Step;
@@ -33,9 +36,77 @@ function CurrentStep({ step, activityId, activeIndex }: props) {
       window.open(step.file, "_blank");
     }
   };
+
+
+
+  const [excelData, setExcelData] = useState([]);
+  const idUser: string = "2";
+  const idActivity: string = "1";
+  const excelURL = "https://docs.google.com/spreadsheets/d/1breEC66LlFSvOqcqz1mxfJglzUVlw6TmArQnezWS27o/edit?usp=drive_link"
+  //otro excel: https://docs.google.com/spreadsheets/d/1SfI2ZN8MUEhpiHlyvuahcLJakrJ08rtjKkWYDYUlTB4/edit?usp=sharing
+
+  const handleExcelImport = async () => {
+    try {
+      const url = excelURL; // Reemplaza con la URL de tu archivo Excel
+    
+      // Realiza una solicitud GET para obtener el archivo Excel
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+        // Lee el archivo Excel y conviértelo en un objeto de datos
+        const excelArrayBuffer = response.data;
+        const workbook = XLSX.read(new Uint8Array(excelArrayBuffer), { type: 'array' });
+        const sheetName = workbook.SheetNames[0]; // Suponiendo que solo hay una hoja en el archivo Excel
+
+        // Convierte la hoja de Excel en un array de objetos JSON
+        const excelArray = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        
+        // Encuentra la última fila no vacía en excelArray
+        let lastNonEmptyRow = null;
+        for (let i = excelArray.length - 1; i >= 0; i--) {
+            if (Object.keys(excelArray[i]).length > 3) {
+                lastNonEmptyRow = excelArray[i];
+                break;
+            }
+        }
+
+        // Verifica si se encontró una fila no vacía
+        if (lastNonEmptyRow) {
+        // Asigna los valores de idUser e idActivity a la última fila no vacía
+        //lastNonEmptyRow.idUser = idUser;
+        //lastNonEmptyRow.idActivity = idActivity;
+        
+        let obj = {
+            gradeValue: lastNonEmptyRow.B,
+            activityId: idActivity,
+            userId: idUser
+        }
+        try {
+            const response = await axios.post('http://localhost:3001/test', obj)
+            console.log(response.data);  
+          } catch (error: any) {
+            console.log(error); 
+        }
+        } else {
+            console.log('No se encontraron filas de datos en el archivo Excel.');
+        }
+
+        console.log(excelArray);
+      // Actualiza el estado con los datos del Excel
+      setExcelData(excelArray);
+    } catch (error) {
+      console.error('Error al importar datos desde Excel:', error);
+    }
+};
+
+useEffect(()=>{
+    return()=>{
+        handleExcelImport()
+    }
+},[])
+
   return (
     <>
-      {step ? (
+      {/* {step ? (
         <div key={step?.number} className="flex my-[5rem] w-[100%]">
           <div className="w-6 mx-5">
             <h1 className="text-bold">{step?.title}</h1>
@@ -69,9 +140,19 @@ function CurrentStep({ step, activityId, activeIndex }: props) {
         </div>
       ) : (
         <h1>Esta actividad no tiene pasos</h1>
-      )}
+      )} */}
+      <div className=' flex items-center justify-center pt-8'>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScez5sHirQ3EgI52ifTCuYaLWS5DzP_OpsxrZ8k1W6sR4ycBQ/viewform?usp=sf_link" 
+          width="640" 
+          height="600" 
+          frameborder="0" 
+          marginheight="0" 
+          marginwidth="0">
+              Cargando…
+        </iframe>
+      </div>
     </>
   );
 }
-
+//otro formulario: https://docs.google.com/forms/d/e/1FAIpQLSfZlqtLmw0XM1JGmvo60f1Nmm7bojPSJIa83ayuq6pWzn1WVQ/viewform?usp=sf_link
 export default CurrentStep;
