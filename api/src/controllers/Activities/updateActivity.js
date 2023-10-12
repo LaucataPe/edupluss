ee7d4f6522fca57b32edfe41a87686b2dcc7ce63
Franco Vedia <francoaresis@gmail.com>
2023-10-12 15:58:41 -0300

    fix(api/src/controllers/updateActivity):this rute was not working for a bug, solved.

--- api/src/controllers/Activities/updateActivity.js
+++ api/src/controllers/Activities/updateActivity.js
@@ -1,24 +1,26 @@
 const { catchedAsync } = require('../../utils');
 const { validateUpdateActivity } = require('./activitySchema');
 const patchActivity = require('./patchActivity');
+const { Activity } = require("../../db");
 
 
 const updateActivity = async (req, res) => {
   try {
-    const validation = validateUpdateActivity(req.body);
+    // const validation = validateUpdateActivity(req.body);
   
-    if(!validation.success){
-      return res.status(400).json({message: validation.error.issues[0].message})
-    }
+    // if(!validation.success){
+    //   return res.status(400).json({message: validation.error.issues[0].message})
+    // }
+    // const activityUpdated = await patchActivity(id, validation.data);
   
-    const { id } = req.body;
-    const activityUpdated = await patchActivity(id, validation.data);
+    // if(typeof activityUpdated === "string"){
+    //   return res.status(404).json({message: activityUpdated})
+    // }
   
-    if(typeof activityUpdated === "string"){
-      return res.status(404).json({message: activityUpdated})
-    }
+    const { id,title } = req.body;
+    const updateActivity = await Activity.update({title},{where:{id}})
   
-    res.status(200).json(activityUpdated)
+    res.status(200).json(updateActivity[0])
   } catch (error) {
     res.status(500).json({error: error})
   }
