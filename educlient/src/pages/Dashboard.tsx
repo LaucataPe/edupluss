e1bfff45e913a3282157fcd5349d9c686eeb4665
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2023-10-11 22:01:17 -0300

    WIP feat(educlient/src/pages/Dashboard) Scatter for users + progress

--- educlient/src/pages/Dashboard.tsx
+++ educlient/src/pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import React, { useEffect, useState } from "react";
 import { Button } from "primereact/button";
 import axios from "axios";
@@ -39,6 +38,7 @@ function Dashboard() {
   const [userSteps, setUserSteps] = useState([]);
   const [userIdCount, setUserIdCount] = useState({});
   const [graduatedUsers, setGraduatedUsers] = useState([]);
+  const [label, setLabel] = useState("Cargando...");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -98,7 +98,7 @@ function Dashboard() {
       try {
         const response = await axios.get("http://localhost:3001/userStep");
         const userSteps = response.data;
-
+        setUserSteps(userSteps);
         const count = {};
 
         userSteps.forEach((userStep: any) => {
@@ -135,21 +135,73 @@ function Dashboard() {
       setGraduatedUsers(graduatedUsers);
     }
   }, [totalUsers, totalStepsByRoleId, userIdCount]);
-
-  // console.log("Cantidad total de usuarios:", totalUsers);
-  // console.warn("Suma Steps en Activity por roleId:", totalStepsByRoleId);
-  console.warn("Total de Usuarios graduados:", graduatedUsers);
   const graduatedCount = graduatedUsers.filter((user) => user.graduated).length;
   const remainingCount = graduatedUsers.length - graduatedCount;
-
   console.log(
-    `Usuarios graduados: ${graduatedCount}, Usuarios faltantes: ${remainingCount}`
+    "totalStepsByRoleId (Maximo de pasos que necesitan tener para finalizar una actividad segun el rol):",
+    totalStepsByRoleId,
+    "totalUsers (Info de usuarios con su roleId adentro):",
+    "userSteps (Progreso total de todos los userId):",
+    userSteps,
+    "userIdCount (Progreso individual por userId): ",
+    userIdCount
   );
+  // console.warn("Suma Steps en Activity por roleId:", totalStepsByRoleId);
+  // console.warn("Total de Usuarios graduados:", graduatedUsers);
+
+  // console.log(
+  //   `Usuarios graduados: ${graduatedCount}, Usuarios faltantes: ${remainingCount}`
+  // );
+
   // #### Grafico 1 Progreso de usuarios ####
+
+  useEffect(() => {
+    if (totalUsers) {
+      const usersWithCompanyId1 = totalUsers.filter(
+        (user) => user.companyId === 1
+      );
+      const usernames = usersWithCompanyId1.map((user) => user.username);
+
+      if (usernames.length > 0) {
+        setLabel(usernames);
+      } else {
+        setLabel("No se encontraron usuarios");
+      }
+    }
+  }, [totalUsers]);
   const generatePositiveRandomData = () => {
     return labels.map(() => Math.max(Math.floor(Math.random() * 200) - 100, 1));
   };
+  useEffect(() => {
+    if (totalUsers && totalStepsByRoleId && userIdCount) {
+      const first5UserIds = [1, 2, 3, 4, 5];
 
+      // Inicializar un objeto para almacenar el progreso de los primeros 5 usuarios
+      const progressData = {};
+
+      // Calcular el progreso para los primeros 5 usuarios
+      first5UserIds.forEach((userId) => {
+        const roleId = totalUsers.find((user) => user.id === userId)?.roleId;
+        const totalSteps = totalStepsByRoleId[roleId] || 0;
+        const individualProgress = userIdCount[userId] || 0;
+
+        // Calcular el progreso individual * 100 / total de pasos requeridos según el rol
+        const progress = (individualProgress * 100) / totalSteps;
+
+        // Utiliza userId como clave en lugar de un índice fijo
+        progressData[`Usuario ${userId}`] = progress;
+      });
+
+      // Actualizar los datos en el objeto data
+      data.datasets[0].data = first5UserIds.map(
+        (userId) => progressData[`Usuario ${userId}`]
+      );
+
+      console.warn(data.datasets[0].data);
+    }
+  }, [totalUsers, totalStepsByRoleId, userIdCount]);
+
+  //Esto es en general, excepto el primer graficos
   const labels = [
     "Actividad 1",
     "Actividad 2",
@@ -160,7 +212,7 @@ function Dashboard() {
     "Actividad 7",
   ];
   const randomData1 = generatePositiveRandomData();
-  const randomData2 = generatePositiveRandomData();
+  // const randomData2 = generatePositiveRandomData();
 
   const sortDataDescending = (data) => {
     return data.slice().sort((a, b) => b - a);
@@ -170,16 +222,10 @@ function Dashboard() {
     labels,
     datasets: [
       {
-        label: "Usuario 1",
+        label: "Usuario",
         data: sortDataDescending(randomData1),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        label: "Usuario 2",
-        data: sortDataDescending(randomData2),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        borderColor: "#9dc065",
+        backgroundColor: "#8ec640",
       },
     ],
   };
@@ -195,14 +241,110 @@ function Dashboard() {
     maintainAspectRatio: false,
     plugins: {
       legend: {
-        position: "right" as const,
+        position: "left" as const,
       },
       title: {
         display: true,
         text: "Progreso de actividades",
       },
+      datalabels: {
+        anchor: "end",
+        align: "end",
+        display: "auto", // Configuración para mostrar los valores encima de las barras
+        color: "black",
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          callback: function (value) {
+            return value + "%";
+          },
+        },
+        grid: {
+          display: false, // Establece display en false para ocultar las líneas de la cuadrícula en el eje X
+        },
+      },
+      y: {
+        max: 100, // Establece el valor máximo en el eje Y en 100
+
+        grid: {
+          display: false, // Establece display en false para ocultar las líneas de la cuadrícula en el eje Y
+        },
+      },
     },
   };
+
+  // #### Grafico 2 Empleados por areas ####
+
+  const randomData3 = generatePositiveRandomData();
+  const randomData4 = generatePositiveRandomData();
+
+  const data2 = {
+    labels,
+    datasets: [
+      {
+        label: "Usuario 3",
+        data: sortDataDescending(randomData3),
+        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgba(75, 192, 192, 0.5)",
+      },
+      {
+        label: "Usuario 4",
+        data: sortDataDescending(randomData4),
+        borderColor: "rgb(255, 205, 86)",
+        backgroundColor: "rgba(255, 205, 86, 0.5)",
+      },
+    ],
+  };
+
+  // #### Grafico 3 Empleados activos y su progreso ####
+
+  function generateRandomData(numPoints) {
+    const data = [];
+
+    for (let i = 0; i < numPoints; i++) {
+      data.push({
+        x: Math.floor(Math.random() * 85),
+        y: Math.floor(Math.random() * 100),
+      });
+    }
+
+    return data;
+  }
+
+  const scatterOptions = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const scatterData = {
+    datasets: [
+      {
+        label: "Usuario",
+        data: generateRandomData(15),
+        backgroundColor: "rgba(255, 99, 132, 1)",
+        pointRadius: 20,
+      },
+      {
+        label: "Actividades",
+        data: generateRandomData(15),
+        backgroundColor: "#0bc8e1",
+        pointRadius: 15,
+      },
+      {
+        label: "Progreso",
+        data: generateRandomData(15),
+        backgroundColor: "#10481a",
+        pointRadius: 5,
+      },
+    ],
+  };
+
+  // ### Escalamiento de graficos ###
 
   const refreshChart = () => {
     setChartKey(chartKey + 1);
@@ -258,72 +400,6 @@ function Dashboard() {
       refreshChart();
     }
   }, [windowDimensions]);
-  // #### Grafico 2 Empleados por areas ####
-  const randomData3 = generatePositiveRandomData();
-  const randomData4 = generatePositiveRandomData();
-
-  const data2 = {
-    labels,
-    datasets: [
-      {
-        label: "Usuario 3",
-        data: sortDataDescending(randomData3),
-        borderColor: "rgb(75, 192, 192)",
-        backgroundColor: "rgba(75, 192, 192, 0.5)",
-      },
-      {
-        label: "Usuario 4",
-        data: sortDataDescending(randomData4),
-        borderColor: "rgb(255, 205, 86)",
-        backgroundColor: "rgba(255, 205, 86, 0.5)",
-      },
-    ],
-  };
-  // #### Grafico 3 Empleados activos y su progreso ####
-
-  function generateRandomData(numPoints) {
-    const data = [];
-
-    for (let i = 0; i < numPoints; i++) {
-      data.push({
-        x: Math.floor(Math.random() * 85),
-        y: Math.floor(Math.random() * 100),
-      });
-    }
-
-    return data;
-  }
-
-  const scatterOptions = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-
-  const scatterData = {
-    datasets: [
-      {
-        label: "Usuario",
-        data: generateRandomData(15),
-        backgroundColor: "rgba(255, 99, 132, 1)",
-        pointRadius: 20,
-      },
-      {
-        label: "Actividades",
-        data: generateRandomData(15),
-        backgroundColor: "#0bc8e1",
-        pointRadius: 15,
-      },
-      {
-        label: "Progreso",
-        data: generateRandomData(15),
-        backgroundColor: "#10481a",
-        pointRadius: 5,
-      },
-    ],
-  };
 
   return (
     <div className="flex">
@@ -351,9 +427,9 @@ function Dashboard() {
             {totalActivities ? totalActivities : "Esperando usuarios..."}
           </h3>
           <h3 className="text-xl font-semibold" style={{ textAlign: "center" }}>
-            Progreso general de Actividades:{" "}
+            Progreso general de Usuarios:{" "}
             <div style={{ display: "flex", justifyContent: "center" }}>
-              <div key={chartKey} style={{ width: "60%", height: "300px" }}>
+              <div key={chartKey} style={{ width: "60%", height: "500px" }}>
                 <Bar options={options} data={data} />
               </div>
             </div>
@@ -361,12 +437,6 @@ function Dashboard() {
             <div style={{ display: "flex", justifyContent: "center" }}>
               <div key={chartKey} style={{ width: "60%", height: "300px" }}>
                 <Bar options={options} data={data2} />
-              </div>
-            </div>
-            Empleados activos y su progreso:{" "}
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <div key={chartKey} style={{ width: "60%", height: "500px" }}>
-                <Scatter options={scatterOptions} data={scatterData} />
               </div>
             </div>
             <div>Acá</div>
