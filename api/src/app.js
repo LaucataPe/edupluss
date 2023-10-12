13a1ef605faf04b8b6cc85a2ce48cded1c01ee5f
Edward Vera <52543389+edwardv1@users.noreply.github.com>
2023-10-12 08:59:57 -0300

    move verifyJWT

--- api/src/app.js
+++ api/src/app.js
@@ -27,7 +27,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/', router); "verifyJWT";
+server.use('/',verifyJWT, router);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
