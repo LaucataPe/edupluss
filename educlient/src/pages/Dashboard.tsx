885f2052770016cd1f92f737e4e2683313116bf0
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2023-10-18 04:48:44 -0300

    refactor(educlient/src/components/Dashboard) Graduate reconfig

--- educlient/src/pages/Dashboard.tsx
+++ educlient/src/pages/Dashboard.tsx
@@ -130,21 +130,26 @@ function Dashboard() {
   useEffect(() => {
     if (totalUsers && totalStepsByRoleId && userIdCount) {
       // Comparación para determinar si un usuario ha "graduado"
-      const graduatedUsers = totalUsers.map((user) => ({
-        userId: user.id,
-        graduated:
-          user.active && // Solo usuarios activos pueden graduarse
-          (user.id in userIdCount ? userIdCount[user.id] : 0) >=
-            (user.roleId in totalStepsByRoleId
-              ? totalStepsByRoleId[user.roleId]
-              : 0),
-      }));
+      const graduatedUsers = totalUsers.map((user) => {
+        const roleIdExists = user.roleId in totalStepsByRoleId;
+        const isEmployee = user.tipo === "empleado"; // Verifica si el usuario es "empleado"
+
+        return {
+          userId: user.id,
+          graduated:
+            user.active && // Solo usuarios activos pueden graduarse
+            isEmployee && // Verifica si el usuario es "empleado"
+            roleIdExists &&
+            (user.id in userIdCount ? userIdCount[user.id] : 0) >=
+              totalStepsByRoleId[user.roleId],
+        };
+      });
       setGraduatedUsers(graduatedUsers);
     }
   }, [totalUsers, totalStepsByRoleId, userIdCount]);
+
   const graduatedCount = graduatedUsers.filter((user) => user.graduated).length;
   const remainingCount = graduatedUsers.length - graduatedCount;
-
   // #### Grafico 1 Progreso de usuarios ####
 
   useEffect(() => {
@@ -248,7 +253,7 @@ function Dashboard() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (logUser && logUser.tipo === "admin") {
+        if (logUser && logUser.tipo === "admin" && areas && totalUsers) {
           const allRoles = [];
 
           for (const area of areas) {
@@ -289,7 +294,6 @@ function Dashboard() {
     fetchData();
   }, [logUser, areas, totalUsers]);
 
-  console.log("employeesByArea", employeesByArea);
   const data2 = {
     labels: Object.keys(employeesByArea), // Nombres de las áreas
     datasets: [
@@ -527,7 +531,7 @@ function Dashboard() {
                       {graduatedCount && remainingCount
                         ? ((graduatedCount / remainingCount) * 100).toFixed(2) +
                           "%"
-                        : "No se puede calcular el porcentaje en este momento."}
+                        : "No disponible."}
                     </div>
                   </div>
                 </div>
@@ -566,6 +570,66 @@ function Dashboard() {
                   </div>
                 </Link>
               </div>
+            </div>
+            <div className="card">
+              <div className="flex align-items-center justify-content-between mb-4">
+                <h5>Notifications</h5>
+              </div>
+              <span className="block text-600 font-medium mb-3">TODAY</span>
+              <ul className="p-0 mx-0 mt-0 mb-4 list-none">
+                <li className="flex align-items-center py-2 border-bottom-1 surface-border">
+                  <div className="w-3rem h-3rem flex align-items-center justify-content-center bg-blue-100 border-circle mr-3 flex-shrink-0">
+                    <i className="pi pi-dollar text-xl text-blue-500" />
+                  </div>
+                  <span className="text-900 line-height-3">
+                    Richard Jones
+                    <span className="text-700">
+                      {" "}
+                      has purchased a blue t-shirt for{" "}
+                      <span className="text-blue-500">79$</span>
+                    </span>
+                  </span>
+                </li>
+                <li className="flex align-items-center py-2">
+                  <div className="w-3rem h-3rem flex align-items-center justify-content-center bg-orange-100 border-circle mr-3 flex-shrink-0">
+                    <i className="pi pi-download text-xl text-orange-500" />
+                  </div>
+                  <span className="text-700 line-height-3">
+                    Your request for withdrawal of{" "}
+                    <span className="text-blue-500 font-medium">2500$</span> has
+                    been initiated.
+                  </span>
+                </li>
+              </ul>
+
+              <span className="block text-600 font-medium mb-3">YESTERDAY</span>
+              <ul className="p-0 m-0 list-none">
+                <li className="flex align-items-center py-2 border-bottom-1 surface-border">
+                  <div className="w-3rem h-3rem flex align-items-center justify-content-center bg-blue-100 border-circle mr-3 flex-shrink-0">
+                    <i className="pi pi-dollar text-xl text-blue-500" />
+                  </div>
+                  <span className="text-900 line-height-3">
+                    Keyser Wick
+                    <span className="text-700">
+                      {" "}
+                      has purchased a black jacket for{" "}
+                      <span className="text-blue-500">59$</span>
+                    </span>
+                  </span>
+                </li>
+                <li className="flex align-items-center py-2 border-bottom-1 surface-border">
+                  <div className="w-3rem h-3rem flex align-items-center justify-content-center bg-pink-100 border-circle mr-3 flex-shrink-0">
+                    <i className="pi pi-question text-xl text-pink-500" />
+                  </div>
+                  <span className="text-900 line-height-3">
+                    Jane Davis
+                    <span className="text-700">
+                      {" "}
+                      has posted a new questions about your product.
+                    </span>
+                  </span>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="flex justify-center">
