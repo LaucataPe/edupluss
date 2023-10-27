94eb02018871eda452be1d3da9a75d1da6bd632f
Edward Vera <edwardismael1@gmail.com>
2023-10-27 14:17:21 -0300

    feat(api/controllers): modify controllers for companies and activities

--- api/src/controllers/Company/updateEmpresa.js
+++ api/src/controllers/Company/updateEmpresa.js
@@ -1,4 +1,4 @@
-const { Company } = require("../../db");
+const { Company, User } = require("../../db");
 const { validateUpdateCompany } = require("./companySchema");
 
 const updateEmpresa = async (req, res) => {
@@ -9,7 +9,7 @@ const updateEmpresa = async (req, res) => {
       return res.status(404).json({ message: validation });
     }
 
-    const { id } = req.body;
+    const { id, active } = req.body;
     const companyFound = await Company.findByPk(id);
 
     if (!companyFound) {
@@ -17,12 +17,31 @@ const updateEmpresa = async (req, res) => {
         .status(404)
         .json({ message: `No existe empresa con id: ${id}` });
     }
+    
+    const usersToChangeActive = await User.findAll({
+      where: {
+        companyId: id,
+      },
+    });
+    if(usersToChangeActive){
+      const promises = usersToChangeActive.map(async (user) => {
+        await User.update(
+          { active: active },
+          {
+            where: {
+              id: user.id
+            }
+          }
+        );
+      });
+      await Promise.all(promises);
+    }
 
     companyFound.set(validation.data).save();
 
-    return res.status(200).json({ message: companyFound });
+    return res.status(200).json(companyFound);
   } catch (error) {
-    return res.status(500).json({ error });
+    return res.status(500).json({ message: error });
   }
 };
 
