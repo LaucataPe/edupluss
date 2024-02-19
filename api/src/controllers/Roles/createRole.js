9e318f875368cf366efd647ce7f3a87a9e6311bd
Laura Perez <laucatape@gmail.com>
2024-02-19 10:58:25 -0500

    Agrego campo de fatherRoleId a modelo de roles para crear rangos

--- api/src/controllers/Roles/createRole.js
+++ api/src/controllers/Roles/createRole.js
@@ -1,30 +1,13 @@
 const { Role, Area } = require('../../db');
 const { catchedAsync } = require('../../utils');
 const createRole = async (req, res) => {
-  const {
-    name,
-    hardSkills,
-    softSkills,
-    schedule,
-    salary,
-    experience,
-    remote,
-    areaId,
-  } = req.body;
+  const { name, hardSkills, softSkills, schedule, salary, experience, remote, areaId, fatherRoleId} = req.body;
   try {
     const area = await Area.findByPk(areaId);
     if (area) {
-      const newRole = await Role.create({
-        name,
-        hardSkills,
-        softSkills,
-        schedule,
-        salary,
-        experience,
-        remote,
-        areaId,
-        companyId: area.companyId,
-      });
+      const newRole = await Role.create({ name, hardSkills, softSkills, schedule, salary, experience, remote, areaId, 
+        companyId: area.companyId, fatherRoleId});
+        
       res.status(200).json(newRole);
     } else {
       throw new Error('No se encontró el área especificada');
